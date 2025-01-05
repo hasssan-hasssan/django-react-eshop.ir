7e8f3f896cec7d4982e060666028d32c3bb912c6
Abolfazl Hassan Zade <77428051+hasssan-hasssan@users.noreply.github.com>
2025-01-05 20:09:41 +0330

    added newOrderAlert signal

--- base/signals.py
+++ base/signals.py
@@ -1,8 +1,9 @@
+import logging
 from django.db.models.signals import pre_save,post_save
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.utils import timezone
-import logging
+from base.models import Order
 
 
 def updateUser(sender, instance, **kwargs):
@@ -35,4 +36,39 @@ def newUserAlert(sender, instance, created, **kwargs):
 
 post_save.connect(newUserAlert, sender=User)
 
+
+def newOrderAlert(sender, instance, created, **kwargs):
+    if created:
+        order = instance
+        orderItems = order.orderitem_set.all()
+        itemsPrice = sum(item.qty * item.price for item in orderItems)
         
+        msg = f"""
+        New order for user: {order.user.username}
+        \n
+        \nOrder: #{order._id}
+        \n----------
+        \n
+        \nOrderItems:
+        """
+        for item in orderItems:
+            msg += f"""
+            \nName : {item.name}
+            \nQty : {item.qty}
+            \nPrice: {item.price}
+            \n-------------------
+            """
+
+        msg += f"\nTotal of items price: {itemsPrice}\n-------------------------------"
+
+        try:
+            send_mail(
+                'New Order on E-Shop',
+                msg,
+                'eshop.hamruyesh@gmail.com',
+                ['abolfazl.hassanzade.81@gmail.com']
+            )
+        except Exception as e:
+            logger.error(f"Error sending email: {e}")
+
+post_save.connect(newOrderAlert, sender=Order)
